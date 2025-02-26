bcaa0e36214390ac6810a7e5fa3166eb987ed71b
wi-dev <developer.wi.76@gmail.com>
2025-02-26 19:12:52 +0900

    feat : replication connect & pool

--- api-v1/auth/src/auth/controller.py
+++ api-v1/auth/src/auth/controller.py
@@ -1,29 +1,41 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from database.app import SessionLocal
+from auth.scheme import UserVerify, UserNewPassword
+from database.app import Master_Session, Slave_Session, Master_Base
 from auth.service import UserCreate, UserService
 
 
-def get_db():
+def get_db(read_only: bool = False):
     # Base.metadata.create_all(bind=engine)
-    db = SessionLocal()
+    if read_only:
+        db = Slave_Session()
+    else:
+        db = Master_Session()
     try:
         yield db
     finally:
         db.close()
 
 
-router_auth = APIRouter(prefix="/auth")
+router_auth = APIRouter(prefix="/auth", tags=["auth"])
 
 
-@router_auth.post("/create", tags=["auth"])
+@router_auth.post("/create")
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
     user_service = UserService(db)
-    return user_service.create_user(user)
+    user = user_service.create_user(user)
+    return user
 
 
-@router_auth.get("/", tags=["auth"])
+@router_auth.post("/verify")
+def verify(user: UserVerify, db: Session = Depends(get_db)):
+    user_service = UserService(db)
+    token = user_service.verify_user(user.email, user.password)
+    return token
+
+
+@router_auth.get("/")
 def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
     user_service = UserService(db)
     user = user_service.get_user(user_id)
@@ -32,10 +44,20 @@ def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
     return user
 
 
-@router_auth.get("/email", tags=["auth"])
+@router_auth.get("/email")
 def get_user_by_email(user_email: str, db: Session = Depends(get_db)):
     user_service = UserService(db)
     user = user_service.get_user_by_email(user_email)
+    print(str(type(user)))
     if str(type(user)) == "<class 'fastapi.exceptions.HTTPException'>":
         raise user
     return user
+
+
+@router_auth.post("/password/change")
+def change_password(user: UserNewPassword, db: Session = Depends(get_db)):
+    user_service = UserService(db)
+    user_service.change_password(
+        email=user.email, password=user.password, new_password=user.new_password
+    )
+    return {"result": "success"}
--- libs/database/src/database/app.py
+++ libs/database/src/database/app.py
@@ -2,10 +2,31 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://test.id:aaaa@127.0.0.1:13306/test_project"
+SQLALCHEMY_MASTER_DATABASE_URL = (
+    "mysql+pymysql://test.id:aaaa@127.0.0.1:13306/test_project"
+)
+SQLALCHEMY_SLAVE_DATABASE_URL = (
+    "mysql+pymysql://test.id:aaaa@127.0.0.1:13307/test_project"
+)
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+master_engine = create_engine(
+    SQLALCHEMY_MASTER_DATABASE_URL,
+    pool_size=5,
+    max_overflow=0,
+    pool_recycle=3600,
+    pool_pre_ping=True,
+)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+slave_engine = create_engine(
+    SQLALCHEMY_SLAVE_DATABASE_URL,
+    pool_size=5,
+    max_overflow=0,
+    pool_recycle=3600,
+    pool_pre_ping=True,
+)
 
-Base = declarative_base()
+Master_Session = sessionmaker(autocommit=False, autoflush=False, bind=master_engine)
+
+Slave_Session = sessionmaker(autocommit=False, autoflush=False, bind=slave_engine)
+
+Master_Base = declarative_base()
