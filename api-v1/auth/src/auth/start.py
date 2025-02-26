2f770a3bbd5e4389b6c396c07f53a9c5690cf8aa
wi-dev <developer.wi.76@gmail.com>
2025-02-26 19:14:17 +0900

    feat : router prefix change

--- api-v1/auth/src/auth/start.py
+++ api-v1/auth/src/auth/start.py
@@ -2,4 +2,4 @@ from auth.controller import router_auth
 from server.app import create_app
 
 app = create_app()
-app.include_router(router_auth, prefix="/api/v1/auth")
+app.include_router(router_auth, prefix="/api/v1")
